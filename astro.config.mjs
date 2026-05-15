f8cd219910ecc79fd8ed76f3fd66797ba00bbbc2
mercury1902 <mercury1902@users.noreply.github.com>
2026-05-15 15:02:15 +0700

    fix: correct @astrojs/vercel import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
